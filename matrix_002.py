c7b697dda3247998b8827a3931aab5a9e4280945
kowaraj <kowaraj4stuff@gmail.com>
2016-05-11 16:29:32 +0200

    second try

---
+++ matrix_002.py
@@ -0,0 +1,8 @@
+# Use Python to find the number of seconds in a year
+answer = 0
+################ YOUR CODE HERE ###############
+
+
+
+############### END OF YOUR CODE ##############
+print "My answer = ", answer
