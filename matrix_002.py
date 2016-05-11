8f25db2f723cc5d1dca281d62fc0e93e26827a2c
Dasha Chekeres <DasHache@Homos-MacBook-Air-2.local>
2016-05-11 16:44:22 +0200

    reshenie

--- matrix_002.py
+++ matrix_002.py
@@ -1,7 +1,7 @@
-# Use Python to find the number of seconds in a year
+# Use Python to find the number of seconds in a year ~10min
 answer = 0
 ################ YOUR CODE HERE ###############
-
+answer = 60*60*24*365
 
 
 ############### END OF YOUR CODE ##############
