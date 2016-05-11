022d13f01351382886fe09597c68f4bbc3f54070
DasHache <dasha.chekeres2@gmail.com>
2016-05-11 19:06:58 +0200

    Division, le reste de division

---
+++ matrix_003.py
@@ -0,0 +1,26 @@
+# Les bases du python / math
+
+# 1. La division (operateur division: /)
+div_12_par_4   = 0 # Trouvez le resultat de la devision 12   par 4
+div_13_par_4   = 0 # Trouvez le resultat de la devision 13   par 4
+div_14p5_par_4 = 0 # Trouvez le resultat de la devision 14.5 par 4
+
+# 2. Le reste d'un division
+reste_div_11_par_4 = 0 # Trouvez le reste d'un division 11 par 4
+
+# 3. Essayez le suivant (operateur reste-de-devision: %)
+print "[rien a faire] Le reste d'une division 11 par 4 = ", 11 % 4
+
+################ YOUR CODE HERE ###############
+
+# div_12_par_4   = 
+# div_13_par_4   = 
+# div_14p5_par_4 =
+# reste_div_11_par_4 = 
+
+############### END OF YOUR CODE ##############
+print "Le resultat d'une division 12 par 4   = ", div_12_par_4
+print "Le resultat d'une division 13 par 4   = ", div_13_par_4 
+print "Le resultat d'une division 14.5 par 4 = ", div_14p5_par_4
+print "Le reste d'une division 11 par 4      = ", reste_div_11_par_4
+
