57f2a0742f3adf73ed22db74bc56b461ce376a66
DasHache <dasha.chekeres2@gmail.com>
2016-05-13 13:39:47 +0200

    boolean operators

---
+++ matrix_004.py
@@ -0,0 +1,34 @@
+# Les bases du python / math
+
+# 1. La verite? Le type qui s'appele Boolean = True ou False
+bool_5_gt_3 = False # Trouvez le resultat d'une operation: 5 plus que 3 (op: >)
+bool_4_lt_2 = False # Trouvez le resultat d...             4 mois que 2 (op: <)
+bool_7_eg_6 = False # Trouvez le resultat d...             7 egal a 6   (op: ==)
+bool_7_ne_6 = False # Trouvez le resultat d...             7 n'est pas egal a 6   (op: !=)
+bool_5_ge_5 = False # Trouvez le resultat d...             5 plus que ou egal a 5 (op: >=)
+bool_4_le_4 = False # Trouvez le resultat d...             4 mois que ou egal a 4 (op: <=)
+
+print "[rien a faire] Question: Est-ce que 12345 est divisible par 3? (sans reste)"
+print "Reponse de '12345 est div par 3' est: ", (12345 % 3) == 0
+
+################ YOUR CODE HERE ###############
+
+bool_5_gt_3 = False
+bool_4_lt_2 = False
+bool_7_eg_6 = False
+bool_7_ne_6 = False
+bool_5_ge_5 = False
+bool_4_le_4 = False
+
+############### END OF YOUR CODE ##############
+
+print "bool_5_gt_3 = ", bool_5_gt_3
+print "bool_4_lt_2 = ", bool_4_lt_2
+print "bool_7_eg_6 = ", bool_7_eg_6
+print "bool_7_ne_6 = ", bool_7_ne_6
+print "bool_5_ge_5 = ", bool_5_ge_5
+print "bool_4_le_4 = ", bool_4_le_4
+
+
+
+
