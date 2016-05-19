6950636ed097b8a5c3a443f9ba42ad4d41d13113
DasHache <dasha.chekeres2@gmail.com>
2016-05-19 12:52:01 +0200

    Dict - 2

---
+++ matrix_010.py
@@ -0,0 +1,48 @@
+# Les bases du python / math
+# Les collections. 6. Les Dictionnaires - 2
+
+# 1.
+# Cree un dict. (il doit contenir au moins 2 paires de   'cle' : valeur)
+# Je veux que ca soit un dict. des acteurs et roles d'un spectacle que vous jouer au theatre
+# comme ca: dasha joue joulette, pasha joue romeo, sasha joue une vase de fleurs
+# mets le dans une variable rj (pour Romeo & Juliet)
+rj = 0   ## <---------------------------------------------------------------- "YOUR CODE HERE"
+
+# 2.
+# Rajoute encore un element dans ton_dict
+rj ...   ## <--------------------------------------------------------------------------- "YOUR CODE HERE"
+print 'Acteurs principaux: ', rj
+
+# 3.
+# Maintenant, cree un dict de remplacants pour chaque role (au cas ou l'acteur principal tombe malade)
+rj_remp = 0    ## <--------------------------------------------------------------------------- "YOUR CODE HERE"
+print 'Acteurs remplacants: ', rj_remp
+
+# 4. 
+# Ca devient plus complique...
+# Cree un troisieme dict qui contient le deux dict precedents - comme les valeur
+# et les cles sont a ton choix
+d_cast = 0     ## <------------------------------------------------- "YOUR CODE HERE"
+print 'Les deux trouppes: ', d_cast
+
+# 5. 
+# En utilisant le dict rj, imprime le nom d'acteur qui joue un role de Romeo
+acteur_romeo = 0   ## <----------------------------------------------------------------------- "YOUR CODE HERE"
+print 'Romeo est: ', acteur_romeo
+
+# 6.
+# En utilisant le dict d_cast, imprime le nom du remplacant pour le role de Juliet
+acteur_juliet = 0  ## <----------------------------------------------------- "YOUR CODE HERE"
+print 'Deuxieme Juliet est: ', acteur_juliet
+
+# 7.
+# Imagine que la famille d'un des acteurs principale vient de demenager
+# et vous avez trouve un autre garcon pour ce role
+# Corrige le dict d_cast pour qu'il soit juste
+d_cast ....   ## <--------------------------------------------------------------------------- "YOUR CODE HERE"
+print 'Le nouveau trouppe est: ', d_cast
+
+
+
+
+
