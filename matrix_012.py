d4721fc2559ed94175c1b2280d4ef7d4c00a6115
DasHache <dasha.chekeres2@gmail.com>
2016-05-22 23:10:32 +0200

    les definitions

---
+++ matrix_012.py
@@ -0,0 +1,45 @@
+# Les bases du python / math
+# Les collections. 7. 'repetitio est mater studiorum'
+
+
+# 1. 
+# Qu'est-ce que c'est La Comprehension? 
+print 'La comprehension est: ', 'TON EXPLICATION ICI'
+# Donne-moi un example de une comprehension
+print '  example = ', 'TON CODE ICI (entre les guillemets)'
+
+# 2.
+# Qu'est-ce que c'est le 'unpacking'
+print 'Le unpacking est: ', 'TON EXPLICATION ICI'
+# Donne-moi un example...
+print '  example = ', 'TON CODE ICI (entre les guillemets)'
+
+# 3.
+# Qu'est-ce que c'est la liste
+print 'La liste est: ', 'TON EXPLICATION ICI'
+# Donne-moi un example...
+print '  example = ', 'TON CODE ICI (entre les guillemets)'
+
+# 4.
+# Qu'est-ce que c'est le set
+print 'Le set est: ', 'TON EXPLICATION ICI'
+# Donne-moi un example...
+print '  example = ', 'TON CODE ICI (entre les guillemets)'
+
+# 5.
+# Qu'est-ce que c'est le dictionnaire
+print 'Le dict est: ', 'TON EXPLICATION ICI'
+# Donne-moi un example...
+print '  example = ', 'TON CODE ICI (entre les guillemets)'
+
+
+
+
+
+
+
+
+
+
+
+
