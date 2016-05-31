b88c38c8b8c8ff1411f6d4a6a5079a5ccc51a962
DasHache <dasha.chekeres2@gmail.com>
2016-05-31 19:43:24 +0200

    functions

---
+++ matrix_015.py
@@ -0,0 +1,81 @@
+# Les bases du python / math
+# Les fonctions.
+
+# Chaque fonction peut avoir:
+# - les arguments 
+# - un objet qu'elle retourne
+#
+# Pour definir (creer) une fonction en Python on utilise le mot 'def'
+# Pour retourner un objet on utilise le mot 'return'
+# Par example:
+def multiply_by_2(x) :
+    return x*2
+# On vient de creer une fonction
+# - qui s'appele: multiply_by_2
+# - qui prend 1 argument: x
+# - qui retourne un object produit par la multiplication de x par 2
+# !! Pour l'instant, le python et la fonction ne savient pas combient vaut x
+#    (En fait, on veut pas le savoir. On s'en fiche.)
+#    Une seul chose important: on prend quelque chose, multiplie ca par 2 et retourne
+# !! La fonction ne le sait qu'au moment ou elle est appele
+#
+# Tu peux utiliser (appeler) cette fonction:
+y = multiply_by_2(10)
+# On vient de creer une variable 'y'
+# et assigner une valuer (qui est retourne par la fonction) a cette variable 'y'
+
+# 1.
+# Definir une fonction:
+# nom (de la fonction)         : plus1
+# input (ce qu'elle prend)     : c'est une entier, X
+# output (ce qu'elle retourne) : c'est une autre entier, qui est 1 plus grande (+1) que
+#                                l'entier d'input
+# <--- TON CODE ICI
+# <--- TON CODE ICI
+
+
+# 2.
+# T'as une liste, L
+L = [1,2,3]
+# Cree une nouvelle liste, chaque element de la quelle
+# est 1 plus grand que l'element correspondant de la liste L
+# Tu dois obtenir une liste Lp1 = [2,3,4]
+# * tu peux utiliser le comprehension
+Lp1 = []
+
+
+
+# 3.
+# Definir une fonction 'nextInts(L)' 
+# son INPUT (ce qu'elle prend):     c'est une liste, L
+# son OUTPUT (ce qu'elle retourne): c'est une autre liste, chaque element de la quelle
+# est plus grande (+1) que l'element correspondant de la liste L
+# C'est a dire, si tu la donne une liste [1,2,3] elle doit retourner [2,3,4]
+# * tu va utiliser Le Comprehension
+# <--- TON CODE ICI
+# <--- TON CODE ICI
+
+# 3.2
+# Utilise ta fonction 'nextInts' avec un argument a ton choix
+# <--- TON CODE ICI
+
+
+# 4.
+# Definir une fonction:
+# - nom    : cubes
+# - input  : une liste, L
+# - output : une liste, chaque element de la quelle est une cube d'element correspondant de la list L
+# - example: input [1,2,3], output [1, 8, 27]
+# <--- TON CODE ICI
+# <--- TON CODE ICI
+
+# 4.2
+# Utilise ta fonction 'cubes' avec un argument a ton choix
+# <--- TON CODE ICI
+
+
+
+
+
+
+
