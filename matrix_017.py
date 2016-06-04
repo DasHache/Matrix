755f8dc60e0daa0ab4359c913ffa89ad53332a23
Dasha Chekeres <DasHache@Homos-MacBook-Air-2.local>
2016-06-04 19:28:10 +0200

    function2

--- matrix_017.py
+++ matrix_017.py
@@ -32,7 +32,7 @@
 # Appele la methode (la fonction) 'parle' de cet objet
 # * rappele: on appele une methode d'un objet en utilisant '.'
 #            comme ca: objet.methode()
-# Ca doit imprimer la prase 'JE NE SAIS PAS CE QUE JE PARLE'
+# Ca doit imprimer la phrase 'JE NE SAIS PAS CE QUE JE PARLE'
 
 
 
@@ -47,7 +47,19 @@
 
 ############################################# TON CODE ICI #############
 
+class Animal :
 
+    def __init__(self, nom):
+        self.name = nom
+    def __str__(self):
+        return  self.name
+    def __parle__(self):
+        return 'JE NE SAIS PAS CE QUE JE PARLE'
+a = Animal('JE NE SAIS PAS MON NOM')
+a.__parle__()
+
+
+print a.__parle__()
 
 
 ########################################################################
